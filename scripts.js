cdf2a04ea3674f4e96aca1aa23364146e3598bc9
shivrajparmar <112109477+shivrajparmar@users.noreply.github.com>
2024-07-28 17:08:15 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,16 +2,41 @@ document.addEventListener('DOMContentLoaded', function() {
     loadPage('page1.html');
 });
 
+let currentPageIndex = 0;
+const pages = ['page1.html', 'page2.html', 'page3.html'];
+
 document.getElementById('btn1').addEventListener('click', function() {
-    loadPage('page1.html');
+    loadPage(pages[0]);
+    currentPageIndex = 0;
+    updateNavButtons();
 });
 
 document.getElementById('btn2').addEventListener('click', function() {
-    loadPage('page2.html');
+    loadPage(pages[1]);
+    currentPageIndex = 1;
+    updateNavButtons();
 });
 
 document.getElementById('btn3').addEventListener('click', function() {
-    loadPage('page3.html');
+    loadPage(pages[2]);
+    currentPageIndex = 2;
+    updateNavButtons();
+});
+
+document.getElementById('prevBtn').addEventListener('click', function() {
+    if (currentPageIndex > 0) {
+        currentPageIndex--;
+        loadPage(pages[currentPageIndex]);
+        updateNavButtons();
+    }
+});
+
+document.getElementById('nextBtn').addEventListener('click', function() {
+    if (currentPageIndex < pages.length - 1) {
+        currentPageIndex++;
+        loadPage(pages[currentPageIndex]);
+        updateNavButtons();
+    }
 });
 
 function loadPage(page) {
@@ -27,4 +52,9 @@ function loadPage(page) {
                 loadLineChart(); // Function to load the line chart visualization
             }
         });
+}
+
+function updateNavButtons() {
+    document.getElementById('prevBtn').disabled = (currentPageIndex === 0);
+    document.getElementById('nextBtn').disabled = (currentPageIndex === pages.length - 1);
 }
